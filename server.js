78e94fc9e8f185f3c06cd3c261ec8b8649f77961
jhahnsheen <91440103+jhahnsheen@users.noreply.github.com>
2022-01-26 13:57:07 -0500

    added product and user

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const sequelize = require('./config/connection');
 
 const app = express();
 
+const User = require('./models/User');
+const Product = require('./models/Product');
+
 const PORT = process.env.PORT || 6969;
 
 app.use(express.json());
